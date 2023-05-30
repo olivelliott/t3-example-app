471ddaaa1c1c8f104eb65e58427834cc6246f01e
Olivia Elliott <olivelliott48@gmail.com>
2023-05-29 20:17:13 -0400

    Rollback clerk middleware method to match ex

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,19 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-export default authMiddleware();
+export default withClerkMiddleware(() => {
+  return NextResponse.next();
+});
 
+// Stop Middleware running on static files
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
 };
+
+// import { authMiddleware } from "@clerk/nextjs";
+
+// export default authMiddleware();
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
