7918a467c38ef41ea7776ffdd591851abfa14634
Olivia Elliott <olivelliott48@gmail.com>
2023-05-29 20:14:21 -0400

    Update router for new Post model and add single Post for testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,9 @@ import { api } from '~/utils/api';
 import { SignIn, SignInButton, SignOutButton, useUser } from '@clerk/nextjs';
 
 const Home: NextPage = () => {
-	const hello = api.example.hello.useQuery({ text: 'from tRPC' });
+	const user = useUser();
 
-    const user = useUser()
+	const { data } = api.posts.getAll.useQuery();
 
 	return (
 		<>
@@ -18,10 +18,15 @@ const Home: NextPage = () => {
 			</Head>
 			<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
 				<div>
-                    {!user.isSignedIn && <SignInButton />}
-                    {!!user.isSignedIn && <SignOutButton />}
-                </div>
-                {/* <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" /> */}
+					{!user.isSignedIn && <SignInButton />}
+					{!!user.isSignedIn && <SignOutButton />}
+				</div>
+				<div>
+					{data?.map((post) => (
+						<div key={post.id}>{post.content}</div>
+					))}
+				</div>
+				{/* <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" /> */}
 			</main>
 		</>
 	);
